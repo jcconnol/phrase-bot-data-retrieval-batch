655438fa3c2a446e5156d69549c340588e18d1a1
John Connolly <jcconnol4@gmail.com>
2023-12-02 16:35:20 -0600

    add final product

---
+++ load_markov_data.py
@@ -0,0 +1,122 @@
+
+import os
+import inflect
+import re
+import psycopg2
+from psycopg2 import pool
+import concurrent.futures
+
+num_threads = 5
+
+db_params = {
+    'dbname': "postgres",
+    'user': "markov_chain_app",
+    'password': "",
+    'host': "192.168.1.113",
+    'port': "5432"
+}
+
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=num_threads,
+    **db_params
+)
+
+def numbers_to_words(input_string):
+    p = inflect.engine()
+    words = input_string.split()
+    words = [p.number_to_words(word) if word.isdigit() else word for word in words]
+    result = ' '.join(words)
+
+    return result
+
+def tokenize_strings(full_string):
+    full_string = full_string.split("_____")[0]
+
+    #remove commas and ()
+    full_string = re.sub(r'[\(\),]', '', full_string)
+
+    # replace .. or ... with .
+    full_string = re.sub(r'\.{2,}', '.', full_string)
+
+    full_string = numbers_to_words(full_string)
+
+    full_string = re.sub(r'[\!?]', '.', full_string)
+
+    full_string = re.sub(r'[^a-zA-Z0-9. ]', '', full_string)
+
+    return \
+        full_string \
+            .upper() \
+            .split()
+
+def clean_tokens(tokenized_files):
+    return tokenized_files
+
+def insert_data(values):
+    connection = None
+    try:
+        query = f"""
+            WITH inserted_key AS (
+                INSERT INTO public.markov_key (category, markov_key)
+                VALUES ('SONG', %s)
+                ON CONFLICT (category,markov_key) DO UPDATE SET category = EXCLUDED.category
+                RETURNING id
+            )
+            INSERT INTO public.markov_value (markov_key_id, markov_value)
+            SELECT id, %s
+            FROM inserted_key;
+        """
+
+        connection = connection_pool.getconn()
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
+
+    except psycopg2.Error as e:
+        # Handle PostgreSQL exceptions here
+        print("Error:", e)
+
+    finally:
+        if connection:
+            connection_pool.putconn(connection)
+
+def process_chunk(chunk):
+    for values in chunk:
+        insert_data(values)
+
+def load_tokens(token_array):
+    threads = []
+
+    tuples_pairs = [(token_array[i], token_array[i + 1]) for i in range(len(token_array) - 1)]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_chunk, tuples_pairs)]
+
+        # Wait for all tasks to complete
+        concurrent.futures.wait(futures)
+
+
+def read_and_load_files_data(directory_path):
+    all_text = ""
+
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                print(file)
+                file_content = f.read()
+                tokenized_content = tokenize_strings(file_content)
+                clean_content = clean_tokens(tokenized_content)
+
+                load_tokens(clean_content)
+
+
+def handler(event, context):
+    directory_path = "C:\\Users\\Jon\\Desktop\\software_eng\\phrase-bot-data-retrieval-batch\\data\\"
+    read_and_load_files_data(directory_path)
+    connection_pool.closeall()
+
+if __name__ == "__main__":
+    handler({},{})
